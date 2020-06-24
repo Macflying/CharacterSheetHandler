776ddd35a24dc70b3092cc71eb8b0bfb67e92cc1
Léo <milborleo@gmail.com>
2020-06-24 22:05:11 +0200

    Add abilities UC and a first draft for abilities listbox.

---
+++ CharacterSheetHandler/Abilities.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace CharacterSheetHandler
+{
+    /// <summary>
+    /// Logique d'interaction pour Abilities.xaml
+    /// </summary>
+    public partial class Abilities : UserControl
+    {
+        public Abilities()
+        {
+            InitializeComponent();
+            DataContext = new List<Skill>()
+            {
+                new Skill("Alertness", 2),
+                new Skill("Crafts", 3),
+            };
+        }
+    }
+
+    public class Skill
+    {
+        public Skill(string name, int level)
+        {
+            Name = name;
+            Level = level;
+        }
+
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
