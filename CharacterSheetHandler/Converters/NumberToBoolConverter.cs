d6f9e26b0766ba091fd6a9e308906f5a128c22e0
Léo <milborleo@gmail.com>
2020-06-24 20:15:48 +0200

    Add a skill gauge UC  with the appropriate ValueConverter

---
+++ CharacterSheetHandler/Converters/NumberToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CharacterSheetHandler.Converters
+{
+    [ValueConversion(typeof(int), typeof(bool), ParameterType = typeof(int))]
+    public class NumberToBoolConverter : BaseValueConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            if (!int.TryParse(value.ToString(), out int testedValue) || !int.TryParse(parameter.ToString(), out int referenceValue))
+                return false;
+
+            return testedValue >= referenceValue;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.Parse(parameter.ToString());
+        }
+    }
+}
---
+++ CharacterSheetHandler/SkillGauge.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CharacterSheetHandler
+{
+    /// <summary>
+    /// Logique d'interaction pour SkillGauge.xaml
+    /// </summary>
+    public partial class SkillGauge : UserControl
+    {
+        #region Level
+
+        public static readonly DependencyProperty LevelProperty =
+            DependencyProperty.Register(
+            nameof(Level), typeof(int),
+            typeof(SkillGauge));
+
+        public int Level
+        {
+            get { return (int)GetValue(LevelProperty); }
+            set { SetValue(LevelProperty, value); }
+        }
+
+        #endregion Level
+
+        public SkillGauge()
+        {
+            InitializeComponent();
+        }
+    }
+}
