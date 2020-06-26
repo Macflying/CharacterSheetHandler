6418db7a15b6faef5f1078b202317ac8bd06266a
Léo <milborleo@gmail.com>
2020-06-27 00:23:23 +0200

    Add text with placeholder text under path gauge.

--- CharacterSheetHandler/Converters/MaxNumberValueConverter.cs
+++ CharacterSheetHandler/Converters/MaxNumberValueConverter.cs
@@ -5,7 +5,7 @@ using System.Windows.Data;
 namespace CharacterSheetHandler.Converters
 {
     [ValueConversion(typeof(int), typeof(int), ParameterType = typeof(int))]
-    internal class MaxNumberValueConverter : BaseValueConverter
+    internal class GetMaxNumberValueConverter : BaseValueConverter
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -15,7 +15,7 @@ namespace CharacterSheetHandler.Converters
             if (!int.TryParse(value.ToString(), out int testedValue) || !int.TryParse(parameter.ToString(), out int referenceValue))
                 return false;
 
-            return Math.Min(testedValue, referenceValue);
+            return Math.Max(testedValue, referenceValue);
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
