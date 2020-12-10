9510c5cde37b21f0d3978de883c723ed4c7ee218
Léo <milborleo@gmail.com>
2020-12-10 23:29:35 +0100

    Add a service to save an entity to a json file.

---
+++ CharacterSheetHandler.Services/JSonFileService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.IO;
+
+namespace CharacterSheetHandler.Services
+{
+    public class JSonFileService<TEntity>
+    {
+        private static JsonSerializerOptions SerializerOptions => new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+        };
+
+        public void Create(TEntity entity, string fileName)
+        {
+            var entityAsText = JsonSerializer.Serialize(entity, SerializerOptions);
+            File.WriteAllText(fileName, entityAsText, Encoding.UTF8);
+        }
+
+        public TEntity Read(string fileName)
+        {
+            var entityAsText = File.ReadAllText(fileName, Encoding.UTF8);
+            return JsonSerializer.Deserialize<TEntity>(entityAsText, SerializerOptions);
+        }
+    }
+}
